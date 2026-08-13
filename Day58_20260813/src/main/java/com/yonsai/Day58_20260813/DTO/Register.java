c9baf9f372d40dfcd1ea2e8a9be986ab4873b344
Spring Developer <user@example.com>
2026-08-13 16:32:51 +0900

    회원가입 및 로그인 기능 추가

---
+++ Day58_20260813/src/main/java/com/yonsai/Day58_20260813/DTO/Register.java
@@ -0,0 +1,62 @@
+package com.yonsai.Day58_20260813.DTO;
+
+public class Register {
+
+    private String phone;
+    private String address;
+    private String email;
+    private String gender;
+    private String id;
+    private String pw;
+
+    public Register() {
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+}
